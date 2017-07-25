11a784a37964cf3d300f274a461ae919d2246ab4
Brad Schmitt <bschmitt@gmail.com>
2017-07-25 14:36:45 -0500

    Update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import json
 import re
 import pymssql
-from collections import defaultdict
 
 with open('param.secret.json') as json_data:
     parms = json.load(json_data)
 
-fieldmap = []
+fieldmap = {}
 
 conn = pymssql.connect(parms['server'], parms['user'], parms['password'], "ARSystem")
 cursor = conn.cursor()
@@ -22,29 +21,27 @@ fields = cursor.fetchall()
 for row in fields:
 
     thisrow = {}
-    # thisrow = {row[0]:row[1]}
-    thisrow = {"id":row[0],
-               "name": row[1]}
-
 
     cursor.execute("select enumId, value from field_enum_values where schemaId=%s and fieldId=%s" % (schemaid, row[0]))
     enums = cursor.fetchall()
     enumerations = {}
 
     field = str(row[1])
-    if re.match("z[0-9][A-Z]",field):
+    # Skipping all the junk fields
+    if re.match("[zZ][0-9][A-Z]",field):
         print("Skipping %s" % row[1])
     else:
+
         print("Enumerating %s" % row[1])
         for enum in enums:
             enumerations[enum[0]] = enum[1]
 
-        thisrow['enums'] = enumerations
+        thisrow['fieldId'] = row[0]
+        thisrow['valueMap'] = enumerations
+        fieldmap["@%s" % str(row[1]).upper()] = thisrow
 
-    fieldmap.append(thisrow)
-    #fieldmap[row[0]]['test'] = 'test'
-    # if len(enums > 0):
-    #     for enum in enums:
 
+
+# Let's print the json
 print(json.dumps(fieldmap, indent=4, sort_keys=True))
 conn.close()
