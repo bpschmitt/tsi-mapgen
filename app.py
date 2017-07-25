91c75f0c06bf69961ded249f1877e39172fafc61
Brad Schmitt <bschmitt@gmail.com>
2017-07-25 14:17:31 -0500

    Update

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
+import re
 import pymssql
+from collections import defaultdict
 
 with open('param.secret.json') as json_data:
     parms = json.load(json_data)
 
-fieldmap = {}
+fieldmap = []
 
 conn = pymssql.connect(parms['server'], parms['user'], parms['password'], "ARSystem")
 cursor = conn.cursor()
@@ -18,25 +20,31 @@ cursor.execute("select fieldId,fieldName from field where schemaId=%s order by f
 fields = cursor.fetchall()
 
 for row in fields:
-    fieldmap[row[0]] = row[1]
 
-    cursor.execute("select enumId, value from field_enum_values where schemaId=%s and fieldId=%s" % (schemaid,row[0]))
+    thisrow = {}
+    # thisrow = {row[0]:row[1]}
+    thisrow = {"id":row[0],
+               "name": row[1]}
+
+
+    cursor.execute("select enumId, value from field_enum_values where schemaId=%s and fieldId=%s" % (schemaid, row[0]))
     enums = cursor.fetchall()
-    print(enums)
+    enumerations = {}
+
+    field = str(row[1])
+    if re.match("z[0-9][A-Z]",field):
+        print("Skipping %s" % row[1])
+    else:
+        print("Enumerating %s" % row[1])
+        for enum in enums:
+            enumerations[enum[0]] = enum[1]
+
+        thisrow['enums'] = enumerations
+
+    fieldmap.append(thisrow)
+    #fieldmap[row[0]]['test'] = 'test'
     # if len(enums > 0):
     #     for enum in enums:
 
-print(json.dumps(fieldmap))
-
-
-# cursor.execute("select enumId, value from field_enum_values where schemaId=1439 and fieldId=1000000162")
-# # you must call commit() to persist your data if you don't set autocommit to True
-# conn.commit()
-
-# cursor.execute('SELECT * FROM persons WHERE salesrep=%s', 'John Doe')
-# row = cursor.fetchone()
-# while row:
-#     print("ID=%d, Name=%s" % (row[0], row[1]))
-#     row = cursor.fetchone()
-
+print(json.dumps(fieldmap, indent=4, sort_keys=True))
 conn.close()
