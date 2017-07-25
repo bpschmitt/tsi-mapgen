2d2dba535a479569e1ec8d6fccd7f230c2fe3475
Brad Schmitt <bschmitt@gmail.com>
2017-07-24 23:48:37 -0500

    Update

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-from os import getenv
+import json
 import pymssql
 
-server = getenv("PYMSSQL_TEST_SERVER")
-user = getenv("PYMSSQL_TEST_USERNAME")
-password = getenv("PYMSSQL_TEST_PASSWORD")
+with open('param.secret.json') as json_data:
+    parms = json.load(json_data)
 
-conn = pymssql.connect(server, user, password, "tempdb")
+fieldmap = {}
+
+conn = pymssql.connect(parms['server'], parms['user'], parms['password'], "ARSystem")
 cursor = conn.cursor()
 
+# Get schemaId
 cursor.execute("select schemaId from arschema where name='HPD:Help Desk'")
-cursor.execute("select fieldId,fieldName from field where schemaId=1439 order by fieldName")
-cursor.execute("select enumId, value from field_enum_values where schemaId=1439 and fieldId=1000000162")
-# you must call commit() to persist your data if you don't set autocommit to True
-conn.commit()
+schemaid = cursor.fetchone()[0]
 
-cursor.execute('SELECT * FROM persons WHERE salesrep=%s', 'John Doe')
-row = cursor.fetchone()
-while row:
-    print("ID=%d, Name=%s" % (row[0], row[1]))
-    row = cursor.fetchone()
+# Get fields
+cursor.execute("select fieldId,fieldName from field where schemaId=%s order by fieldName" % schemaid)
+fields = cursor.fetchall()
+
+for row in fields:
+    fieldmap[row[0]] = row[1]
+
+    cursor.execute("select enumId, value from field_enum_values where schemaId=%s and fieldId=%s" % (schemaid,row[0]))
+    enums = cursor.fetchall()
+    print(enums)
+    # if len(enums > 0):
+    #     for enum in enums:
+
+print(json.dumps(fieldmap))
+
+
+# cursor.execute("select enumId, value from field_enum_values where schemaId=1439 and fieldId=1000000162")
+# # you must call commit() to persist your data if you don't set autocommit to True
+# conn.commit()
+
+# cursor.execute('SELECT * FROM persons WHERE salesrep=%s', 'John Doe')
+# row = cursor.fetchone()
+# while row:
+#     print("ID=%d, Name=%s" % (row[0], row[1]))
+#     row = cursor.fetchone()
 
 conn.close()
